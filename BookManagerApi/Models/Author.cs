46bac33fba3e630a2113acaed137674789cd025e
kennychiangcom <kenny@chiang.com>
2022-10-23 02:03:25 +0100

    Completed Assignment - API Lab 2 - Connecting to Real Databases. In this repo: - Connected to local MySQL database - Created different appsettings.json files - Created different launch profiles

---
+++ BookManagerApi/Models/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace BookManagerApi.Models
+{
+    public class Author
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string Description { get; set; }
+        public string BriefBio { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ BookManagerApi/Models/AuthorContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookManagerApi.Models
+{
+    public class AuthorContext : DbContext
+    {
+        public AuthorContext(DbContextOptions<AuthorContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+
+    }
+}
